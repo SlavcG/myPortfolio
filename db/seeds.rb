e8f52579202276744eff00cf2cbe9f45966236e9
SlavcG <slavkogalich@gmail.com>
2017-03-27 14:40:23 +0200

    Implemented show functionality for portfolio module

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ puts "5 skills created!"
     title: "Portfolio title: #{portfolio}",
     subtitle: "My great service",
     body: "Some random text for body",
-    main_image: "http://placehold.it/600x200",
+    main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200",
   )
 end
